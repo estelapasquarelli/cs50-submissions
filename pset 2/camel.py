f740632e8b491ef067009b1e4876841b6082de28
Estela Pasquarelli <estelapasq@gmail.com>
2025-07-16 22:00:03 -0300

    Add files via upload

---
+++ pset 2/camel.py
@@ -0,0 +1,15 @@
+def camel_to_snake(text):
+    result = []
+    for char in text:
+        if char.isupper():
+            result.append("_")
+            result.append(char.lower())
+        else:
+            result.append(char)
+
+    return "".join(result)
+
+text = str(input("camelCase: "))
+
+print("snake_case: " + camel_to_snake(text))
+
