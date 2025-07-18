85d431701a5348cfcb7143f8077e93c895782d36
Estela Pasquarelli <estelapasq@gmail.com>
2025-07-18 13:53:24 -0300

    Update twttr.py

--- pset 2/twttr.py
+++ pset 2/twttr.py
@@ -1,7 +1,9 @@
 #remover letras específicas
 def remove_vowels(word):
     vowels = "aeiouAEIOU"
-    return "".join(letter for letter in word if letter not in vowels)
+    return "".join(letter for letter in word if letter not in vowels) 
+
+#.join(): usado para juntar uma sequência de strings (como uma lista ou tupla) em uma única string, usando um separador definido
 
 word = str(input("Input: "))
 print("Output: " + remove_vowels(word))
